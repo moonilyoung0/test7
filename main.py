418ffadecdb558e57b0478813b8d0fe5f1ec5f2c
moonilyoung0 <106159159+moonilyoung0@users.noreply.github.com>
2022-05-30 03:40:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request
-import test_model
 
 app = Flask(__name__)
 
@@ -9,12 +8,7 @@ def predict():
     if request.method == "POST":
         firebase_url = request.form["firebase_url"]
         print(firebase_url)
-
-        video = test_model.download_video(firebase_url)
-        print(video)
-        
-        res = test_model.predict_video(video)
-        print(res)
+        res = firebase_url
         
         return render_template("index.html", result = res)
     return render_template("index.html")
